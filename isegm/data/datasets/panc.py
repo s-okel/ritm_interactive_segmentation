9b493524b5166d168624621dffa98bdee511170f
Sanne Okel <sanne.okel@philips.com>
2022-08-01 09:52:07 +0200

    Code for checking dataset

--- isegm/data/datasets/panc.py
+++ isegm/data/datasets/panc.py
@@ -1,19 +1,20 @@
 import cv2
 from isegm.data.base import ISDataset
 from isegm.data.sample import DSample
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
 
 class PancDataset(ISDataset):
-    def __init__(self, split, label, one_input_channel=False, **kwargs):
+    def __init__(self, split, label, one_input_channel=False, data_path="./datasets/Panc/", **kwargs):
         super(PancDataset, self).__init__(**kwargs)
-        assert split in ['train', 'val']
+        assert split in ['train', 'val', 'test']
         self.name = "Panc"
         self.one_input_channel = one_input_channel
 
-        self.data = torch.load(f"./datasets/Panc/{label}_{split}_slices.pt")
+        self.data = torch.load(data_path + f"{label}_{split}_slices.pt")
         self.dataset_samples = range(len(self.data[0]))
 
     def get_sample(self, index) -> DSample:
@@ -26,7 +27,22 @@ class PancDataset(ISDataset):
 
 
 if __name__ == "__main__":
-    dataset = PancDataset('val', 'pancreas', one_input_channel=False)
-    dataloader = DataLoader(dataset)
+    lbl = "arteria_mesenterica_superior"
+    dataset = PancDataset('val', lbl, one_input_channel=False,
+                          data_path="C:/Users/320151982/source/repos/ritm_interactive_segmentation/datasets/Panc/")
+    dataloader = DataLoader(dataset, shuffle=True)
     x = next(iter(dataloader))
-    print(x)
+    print(x['images'].shape)
+    print(x['points'].shape)
+    print(x['instances'].shape)
+    print(x['images'][0].shape)
+
+    image = torch.moveaxis(x['images'][0], 0, -1)
+    instance = x['instances'][0, 0]
+
+    f, axs = plt.subplots(1, 2)
+    axs[0].imshow(image)
+    axs[1].imshow(image)
+    axs[1].imshow(instance, alpha=0.2 * instance, cmap="Reds")
+    plt.show()
+
