240448af3882901d13f9e5cf247795d74592f4fd
Sanne Okel <sanne.okel@philips.com>
2022-08-10 10:42:08 +0200

    One plot per function

--- visualize_results.py
+++ visualize_results.py
@@ -5,174 +5,12 @@ import os
 import pandas as pd
 import pickle
 import regex as re
-import scipy.stats as st
 from termcolor import colored
-from isegm.inference.utils import compute_noc_metric
-
-
-def plots(ious_array, n_clicks, lw=0.5, save=False, font_size=12):
-    # statistics
-    mean = np.mean(ious_array, axis=0)
-    std = np.std(ious_array, axis=0)
-    ci = st.t.interval(0.95, ious_array.shape[0], loc=mean, scale=st.sem(ious_array, axis=0))
-
-    # boxplots
-    f, ax = plt.subplots()
-    ax.boxplot(ious_array[:, :n_clicks], showfliers=False, medianprops=dict(color="#e28743"))
-    plt.xlabel("Amount of clicks", fontsize=font_size)
-    plt.ylabel("mIoU", fontsize=font_size)
-    plt.ylim([0, 1])
-    ax.spines.right.set_visible(False)
-    ax.spines.top.set_visible(False)
-    if save:
-        plt.savefig("epoch_evaluations/individual_boxplot.pdf", dpi=300)
-    else:
-        plt.show()
-
-    # ci
-    ''' 
-    f, ax = plt.subplots()
-    ax.plot(mean, color="#e28743", linewidth=lw)
-    ax.plot(ci[0], color="#eab676", linewidth=lw)
-    ax.plot(ci[1], color="#eab676", linewidth=lw)
-    ax.spines.right.set_visible(False)
-    ax.spines.top.set_visible(False)
-    plt.fill_between(range(50), ci[0], ci[1], color="#f3cfb4", alpha=0.5)
-    plt.xlabel("Amount of clicks")
-    plt.ylabel("mIoU")
-    plt.ylim([0, 1])
-        if save:
-        plt.savefig("epoch_evaluations/individual_ci.pdf", dpi=300)
-    else:
-        plt.show()
-    '''
-
-    # with +- one std
-    f, ax = plt.subplots()
-    ax.plot(range(1, 51), mean, color="#e28743", linewidth=lw)
-    ax.plot(range(1, 51), mean - std, color="#eab676", linewidth=lw)
-    ax.plot(range(1, 51), mean + std, color="#eab676", linewidth=lw)
-    ax.spines.right.set_visible(False)
-    ax.spines.top.set_visible(False)
-    plt.fill_between(range(1, 51), mean - std, mean + std, color="#f3cfb4", alpha=0.5)
-    plt.xlabel("Amount of clicks", fontsize=font_size)
-    plt.ylabel("mIoU", fontsize=font_size)
-    plt.ylim([0, 1])
-    plt.xlim([1, 50])
-    if save:
-        plt.savefig("epoch_evaluations/individual_std.pdf", dpi=300)
-    else:
-        plt.show()
-
-    # histogram: how many clicks to 80, 90, 95
-    f, ax = plt.subplots()
-
-    nocs = []
-
-    for i in range(ious_array.shape[0]):
-        noc = get_noc(ious_array[i], 0.8, ious_array.shape[1])
-        nocs.append(noc)
-
-    print(f"NoC@80: {np.mean(nocs)}")
-
-    # noc_list, over_max_list = compute_noc_metric(ious_array, [0.8], max_clicks=50)
-    # print(noc_list)
-
-    ax.hist(nocs, bins=ious_array.shape[1], histtype='step', color="#e28743", lw=0.5)
-    ax.spines.right.set_visible(False)
-    ax.spines.top.set_visible(False)
-    plt.xlabel("Amount of clicks", fontsize=font_size)
-    plt.xlim([0, 51])
-    if save:
-        plt.savefig("epoch_evaluations/individual_histogram.pdf", dpi=300)
-    else:
-        plt.show()
 
 
 def get_noc(iou_arr, iou_thr, max_clicks):
     vals = iou_arr >= iou_thr
     return np.argmax(vals) + 1 if np.any(vals) else max_clicks
-
-
-def combined_plot(data_dict, save=False, font_size=12):
-    f, ax = plt.subplots()
-    colors = ['#332288', '#88CCEE', '#117733', "#e28743", '#DDCC77', '#44AA99', '#CC6677', '#882255', '#AA4499']
-    # colors = ['#8c510a','#d8b365','#f6e8c3','#c7eae5','#5ab4ac','#01665e']
-    selected_colors = colors[0:len(data_dict)]
-    for k, color in zip(data_dict, selected_colors):
-        ax.plot(range(1, 51), np.mean(data_dict[k], axis=0), linewidth=1, label=k, color=color)
-    ax.spines.right.set_visible(False)
-    ax.spines.top.set_visible(False)
-    plt.xlabel("Amount of clicks", fontsize=font_size)
-    plt.ylabel("mIoU", fontsize=font_size)
-    plt.ylim([0, 1])
-    plt.xlim([1, 50])
-    plt.legend(prop={'size': font_size})
-    plt.grid(visible=True, which='both', axis='both')
-    if save:
-        plt.savefig("epoch_evaluations/mIoU_combined.pdf", dpi=300)
-    else:
-        plt.show()
-
-    f2, ax2 = plt.subplots()
-    threshold = 0.8
-
-    for k, color in zip(data_dict, selected_colors):
-        nocs = []
-
-        for i in range(data_dict[k].shape[0]):
-            noc = get_noc(data_dict[k][i], threshold, data_dict[k].shape[1])
-            nocs.append(noc)
-
-        ax2.hist(nocs, bins=data_dict[k].shape[1], histtype='step', label=k, color=color, density=True)
-    ax2.spines.right.set_visible(False)
-    ax2.spines.top.set_visible(False)
-    plt.xlabel(f"Amount of clicks for NoC of {threshold}")
-    plt.ylim([0, 1])
-    plt.xlim([0, 51])
-    plt.ylabel("Density")
-    plt.legend()
-    plt.show()
-
-
-def plot_avg_mask_influence(data_dict, structures_dict, save=False, font_size=12):
-    avg_mask_list = []
-    avg_nocs = []
-    keys = []
-    noc_threshold = 0.8
-
-    for k in data_dict:
-        keys.append(k)
-        # return to the original labels
-        label = k.lower()
-        label = label.replace("superior mesenteric artery", "arteria mesenterica superior")
-        label = label.replace(" ", "_")
-
-        nocs = []
-        avg_mask_list.append(structures_dict[label]['avg_mask'])
-        for i in range(data_dict[k].shape[0]):
-            noc = get_noc(data_dict[k][i], noc_threshold, data_dict[k].shape[1])
-            nocs.append(noc)
-        avg_nocs.append(np.mean(np.array(nocs)))
-
-    f, ax = plt.subplots()
-    ax.plot(avg_mask_list, avg_nocs, 'x', color="#e28743")
-    for i in range(len(avg_mask_list)):
-        if "Tumour" in keys[i]:
-            plt.text(avg_mask_list[i] + 70, avg_nocs[i] - 1, keys[i], fontsize=font_size)
-        else:
-            plt.text(avg_mask_list[i] + 70, avg_nocs[i], keys[i], fontsize=font_size)
-    ax.spines.right.set_visible(False)
-    ax.spines.top.set_visible(False)
-    plt.ylim([0, math.ceil(np.max(avg_nocs)) + 1])
-    plt.xlabel("Average mask size", fontsize=font_size)
-    plt.ylabel(f"NoC@{int(100 * noc_threshold)}", fontsize=font_size)
-    plt.xlim([0, 3500])
-    plt.grid(visible=True, which='both', axis='both')
-    if save:
-        plt.savefig("epoch_evaluations/noc_vs_mask_size.pdf", dpi=300)
-    else:
-        plt.show()
 
 
 def load_data_to_plot(data_dict):
@@ -190,10 +28,10 @@ def load_data_to_plot(data_dict):
                         for file in os.listdir(plots_path):
                             for k in data_dict:
                                 if ".pickle" in file:
-                                    if data_dict[k]['try'] in plots_path and str(
-                                            data_dict[k]['epoch']) in plots_path and k in plots_path and '1' + str(
-                                            data_dict[k]['epoch']) not in plots_path and '2' + str(
-                                            data_dict[k]['epoch']) not in plots_path:
+                                    if data_dict[k]['try'] in plots_path \
+                                            and 'epoch-' + str(data_dict[k]['epoch']) in plots_path \
+                                            and k in plots_path and '1' + str(data_dict[k]['epoch']) not in plots_path \
+                                            and '2' + str(data_dict[k]['epoch']) not in plots_path:
                                         print(colored(f"Key: {k}", "green"))
                                         print(f"Loading {plots_path + file}")
                                         with open(plots_path + file, "rb") as f:
@@ -267,57 +105,220 @@ def process_results_txt():
             df.to_excel(f"epoch_evaluations/model_{model}_try_{try_nr}.xlsx", index=False, header=False)
 
 
-def plot_delta(data_dict, save=False, font_size=12):
+def single_boxplot(data_dict, label, n_clicks, save=False, font_size=12):
+    ious_array = data_dict[label][:, :n_clicks]
+
+    f, ax = plt.subplots()
+    ax.boxplot(ious_array, showfliers=False, medianprops=dict(color="#e28743"))
+    plt.xlabel("Amount of clicks", fontsize=font_size)
+    plt.ylabel("mIoU", fontsize=font_size)
+    plt.ylim([0, 1])
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    if save:
+        plt.savefig(f"epoch_evaluations/{label}_single_boxplot.pdf", dpi=300)
+    else:
+        plt.show()
+
+
+def single_std_plot(data_dict, label, n_clicks, lw=0.5, save=False, font_size=12):
+    ious_array = data_dict[label][:, :n_clicks]
+    mean = np.mean(ious_array, axis=0)
+    std = np.std(ious_array, axis=0)
+
+    f, ax = plt.subplots()
+    ax.plot(range(1, n_clicks + 1), mean, color="#e28743", linewidth=lw)
+    ax.plot(range(1, n_clicks + 1), mean - std, color="#eab676", linewidth=lw)
+    ax.plot(range(1, n_clicks + 1), mean + std, color="#eab676", linewidth=lw)
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    plt.fill_between(range(1, n_clicks + 1), mean - std, mean + std, color="#f3cfb4", alpha=0.5)
+    plt.xlabel("Amount of clicks", fontsize=font_size)
+    plt.ylabel("mIoU", fontsize=font_size)
+    plt.ylim([0, 1])
+    plt.xlim([1, n_clicks])
+    if save:
+        plt.savefig(f"epoch_evaluations/{label}_individual_std.pdf", dpi=300)
+    else:
+        plt.show()
+
+
+def single_noc_histogram(data_dict, label, n_clicks, noc_thr, lw=0.5, save=False, font_size=12):
+    ious_array = data_dict[label][:, :n_clicks]
+    nocs = []
+
+    for i in range(ious_array.shape[0]):
+        noc = get_noc(ious_array[i], iou_thr=0.8, max_clicks=n_clicks)
+        nocs.append(noc)
+
+    print(f"NoC@{int(noc_thr * 100)}: {np.mean(nocs)}")
+
+    # noc_list, over_max_list = compute_noc_metric(ious_array, [noc_thr], max_clicks=n_clicks)
+    # print(noc_list)
+
+    f, ax = plt.subplots()
+    ax.hist(nocs, bins=n_clicks, histtype='step', color="#e28743", lw=lw)
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    plt.xlabel("Amount of clicks", fontsize=font_size)
+    plt.xlim([0, n_clicks + 1])
+    if save:
+        plt.savefig(f"epoch_evaluations/{label}_individual_histogram.pdf", dpi=300)
+    else:
+        plt.show()
+
+
+def combined_miou_plot(data_dict, n_clicks, lw=0.5, save=False, font_size=12):
+    f, ax = plt.subplots()
     colors = ['#332288', '#88CCEE', '#117733', "#e28743", '#DDCC77', '#44AA99', '#CC6677', '#882255', '#AA4499']
     # colors = ['#8c510a','#d8b365','#f6e8c3','#c7eae5','#5ab4ac','#01665e']
+    selected_colors = colors[0:len(data_dict)]
+    for k, color in zip(data_dict, selected_colors):
+        ax.plot(range(1, n_clicks + 1), np.mean(data_dict[k][:, :n_clicks], axis=0), linewidth=lw, label=k, color=color)
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    plt.xlabel("Amount of clicks", fontsize=font_size)
+    plt.ylabel("mIoU", fontsize=font_size)
+    plt.ylim([0, 1])
+    plt.xlim([1, n_clicks])
+    plt.legend(prop={'size': font_size})
+    plt.grid(visible=True, which='both', axis='both')
+    if save:
+        plt.savefig("epoch_evaluations/mIoU_combined.pdf", dpi=300)
+    else:
+        plt.show()
+
+
+def combined_noc_histogram(data_dict, n_clicks, noc_thr, lw=0.5, save=False, font_size=12):
+    f, ax = plt.subplots()
+    colors = ['#332288', '#88CCEE', '#117733', "#e28743", '#DDCC77', '#44AA99', '#CC6677', '#882255', '#AA4499']
+    selected_colors = colors[0:len(data_dict)]
+
+    for k, color in zip(data_dict, selected_colors):
+        nocs = []
+        ious_array = data_dict[k][:, :n_clicks]
+
+        for i in range(ious_array.shape[0]):
+            noc = get_noc(ious_array[i], noc_thr, n_clicks)
+            nocs.append(noc)
+
+        ax.hist(nocs, bins=n_clicks, histtype='step', label=k, color=color, density=True, lw=lw)
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    plt.xlabel(f"Amount of clicks for NoC of {noc_thr}", fontsize=font_size)
+    plt.ylim([0, 1])
+    plt.xlim([0, n_clicks + 1])
+    plt.ylabel("Density", fontsize=font_size)
+    plt.legend()
+    if save:
+        plt.savefig("epoch_evaluations/nocs_combined.pdf", dpi=300)
+    else:
+        plt.show()
+
+
+def plot_avg_mask_influence(data_dict, structures_dict, noc_thr, lw=0.5, save=False, font_size=12):
+    avg_mask_list = []
+    avg_nocs = []
+    keys = []
+
+    for k in data_dict:
+        keys.append(k)
+        # return to the original labels
+        label = k.lower()
+        label = label.replace("superior mesenteric artery", "arteria mesenterica superior")
+        label = label.replace(" ", "_")
+
+        nocs = []
+        avg_mask_list.append(structures_dict[label]['avg_mask'])
+        for i in range(data_dict[k].shape[0]):
+            noc = get_noc(data_dict[k][i], noc_thr, data_dict[k].shape[1])
+            nocs.append(noc)
+        avg_nocs.append(np.mean(np.array(nocs)))
+
+    f, ax = plt.subplots()
+    ax.plot(avg_mask_list, avg_nocs, 'x', color="#e28743", lw=lw)
+    for i in range(len(avg_mask_list)):
+        if "Tumour" in keys[i]:
+            plt.text(avg_mask_list[i] + 70, avg_nocs[i] - 1, keys[i], fontsize=font_size)
+        else:
+            plt.text(avg_mask_list[i] + 70, avg_nocs[i], keys[i], fontsize=font_size)
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    plt.ylim([0, math.ceil(np.max(avg_nocs)) + 1])
+    plt.xlabel("Average mask size", fontsize=font_size)
+    plt.ylabel(f"NoC@{int(100 * noc_thr)}", fontsize=font_size)
+    plt.xlim([0, 3500])
+    plt.grid(visible=True, which='both', axis='both')
+    if save:
+        plt.savefig("epoch_evaluations/noc_vs_mask_size.pdf", dpi=300)
+    else:
+        plt.show()
+
+
+def combined_delta_relative(data_dict, n_clicks, lw=0.5, save=False, font_size=12):
+    colors = ['#332288', '#88CCEE', '#117733', "#e28743", '#DDCC77', '#44AA99', '#CC6677', '#882255', '#AA4499']
     selected_colors = colors[0:len(data_dict)]
 
     f, ax = plt.subplots()
     for k, color in zip(data_dict, selected_colors):
-        improvement = np.mean(data_dict[k] - np.mean(data_dict[k][:, 0]), axis=0)
-        ax.plot(range(1, 51), improvement, linewidth=1, label=k, color=color)
-        print(f"{improvement[49]:.2f} improvement after 50 clicks for {k}")
+        ious_array = data_dict[k][:, :n_clicks]
+        improvement = np.mean(ious_array - np.mean(ious_array[:, 0]), axis=0)
+        ax.plot(range(1, n_clicks + 1), improvement, label=k, color=color, lw=lw)
+        print(f"{improvement[9]:.2f} improvement after 10 clicks for {k}")
     ax.spines.right.set_visible(False)
     ax.spines.top.set_visible(False)
     plt.xlabel("Number of clicks", fontsize=font_size)
     plt.ylabel(f"Increase in mIoU", fontsize=font_size)
-    plt.xlim([1, 50])
+    plt.xlim([1, n_clicks])
     plt.legend(prop={'size': font_size})
     plt.grid(visible=True, which='both', axis='both')
     if save:
-        plt.savefig("epoch_evaluations/delta.pdf", dpi=300)
+        plt.savefig("epoch_evaluations/delta_relative.pdf", dpi=300)
     else:
         plt.show()
 
+
+def combined_delta_absolute(data_dict, n_clicks, lw=0.5, save=False, font_size=12):
+    colors = ['#332288', '#88CCEE', '#117733', "#e28743", '#DDCC77', '#44AA99', '#CC6677', '#882255', '#AA4499']
+    selected_colors = colors[0:len(data_dict)]
+
     f, ax = plt.subplots()
-    plt.hlines(0, 0, 49, colors=['k'], lw=1)
+    plt.hlines(0, 1, n_clicks, colors=['k'], lw=lw)
     for k, color in zip(data_dict, selected_colors):
-        ax.plot(range(1, 50), np.diff(np.mean(data_dict[k], axis=0)), linewidth=1, label=k, color=color)
+        ious_array = data_dict[k][:, :n_clicks]
+        ax.plot(range(1, n_clicks), np.diff(np.mean(ious_array, axis=0)), linewidth=lw, label=k, color=color)
     ax.spines.right.set_visible(False)
     ax.spines.top.set_visible(False)
-    plt.xlabel("Number of clicks")
-    plt.ylabel(f"Delta")
-    plt.xlim([1, 49])
+    plt.xlabel("Number of clicks", fontsize=font_size)
+    plt.ylabel(f"Delta", fontsize=font_size)
+    plt.xlim([1, n_clicks - 1])
     plt.legend()
-
-    plt.show()
+    if save:
+        plt.savefig("epoch_evaluations/delta_absolute.pdf", dpi=300)
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
     experiments_path = "Z:/Pancreas/interactivity/repos/ritm_interactive_segmentation/experiments/iter_mask/"
     fs = 13
-    process_results_txt()
+    # process_results_txt()
 
     # final: aorta, SMA, PD
+    # semi final: CBD, GA, pancreas, tumour
     structures = {'aorta': {'try': '001', 'epoch': 169, 'avg_mask': 3001},
                   'arteria_mesenterica_superior': {'try': '001', 'epoch': 119, 'avg_mask': 252},
-                  'common_bile_duct': {'try': '001', 'epoch': 30, 'avg_mask': 501},
-                  'gastroduodenalis': {'try': '001', 'epoch': 149, 'avg_mask': 61},
+                  'common_bile_duct': {'try': '001', 'epoch': 110, 'avg_mask': 501},
+                  'gastroduodenalis': {'try': '001', 'epoch': 29, 'avg_mask': 61},
                   'pancreas': {'try': '002', 'epoch': 149, 'avg_mask': 979},
                   'pancreatic_duct': {'try': '000', 'epoch': 79, 'avg_mask': 162},
-                  'tumour': {'try': '000', 'epoch': 29, 'avg_mask': 75}}
-    # data = load_data_to_plot(structures)
-    # plot_delta(data, save=True, font_size=fs)
-    # combined_plot(data, save=True, font_size=fs)
-    # plot_avg_mask_influence(data, structures, save=True, font_size=fs)
-    # plots(data['Pancreas'], n_clicks=20, lw=0.5, save=False, font_size=fs)
+                  'tumour': {'try': '000', 'epoch': 39, 'avg_mask': 75}}
+
+    data = load_data_to_plot(structures)
+    plot_avg_mask_influence(data, structures, noc_thr=0.8, save=True, font_size=fs)
+    single_noc_histogram(data, 'Pancreas', n_clicks=50, noc_thr=0.8, lw=0.5, save=False, font_size=fs)
+    combined_miou_plot(data, n_clicks=30, lw=0.5, font_size=fs)
+    combined_noc_histogram(data, n_clicks=50, noc_thr=0.8)
+    combined_noc_histogram(data, n_clicks=30, noc_thr=0.8)
+    combined_delta_absolute(data, n_clicks=50)
+    combined_delta_absolute(data, n_clicks=30)
